e4f436e7a79763e8e10b50b6c84326c573f06d1a
MatiMonas <matutin97@gmail.com>
2024-08-30 20:51:20 -0300

    refactor(infrastructure/http/errohandler)

--- src/infrastructure/http/errorHandler.ts
+++ src/infrastructure/http/errorHandler.ts
@@ -1,29 +1,28 @@
 import env from 'utils/env';
 import { NextFunction, Request, Response } from 'express';
-import { ValidationError } from 'errors';
+import {
+  ExceptionError,
+  ExceptionValidationError,
+} from 'errors/exceptionErrors';
 
 const { NODE_ENV } = env;
 
-interface ApiError extends Error {
-  statusCode?: number;
-}
 export default function errorHandler(
-  error: ApiError,
+  error: ExceptionError,
   req: Request,
   res: Response,
   next: NextFunction
 ) {
-  if (error instanceof ValidationError) {
+  if (error instanceof ExceptionValidationError) {
     return res.status(400).json({ code: 400, errors: error.errors });
   }
 
   let { statusCode = 500, message } = error;
 
-  // TODO: rechek
-  if (statusCode >= 500) {
+  if (statusCode === 500) {
     if (NODE_ENV !== 'production') console.log(error);
 
-    message = 'An unexpected error has ocurred';
+    message = 'Something went wrong.';
   }
-  return res.status(statusCode).json({ status: 'failed', data: message });
+  return res.status(statusCode).json({ code: statusCode, data: message });
 }
