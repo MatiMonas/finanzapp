50cb529b7d7b41e677dc4751d63c300b1d7b7af2
MatiMonas <matutin97@gmail.com>
2024-08-31 00:05:26 -0300

    change(src/container/container.interface): imported file path

--- src/container/container.interface.ts
+++ src/container/container.interface.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client';
 import { RedisClientType } from 'redis';
 import UserUseCase from 'users/usecase';
 import UserRouter from 'users/http/router';
-import UserRepository from 'users/repository/mysql-repository';
+import UserRepository from 'users/repository/user-repository';
 
 type ContainerServices = {
   prismaClient: PrismaClient;
