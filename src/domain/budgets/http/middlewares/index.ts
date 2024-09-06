c96e744cc9e79b88f20aaad041f9bdf824264eb8
MatiMonas <matutin97@gmail.com>
2024-09-06 00:02:29 -0300

    add(budgets/http/middlewares): createBudgetMiddleware

--- src/domain/budgets/http/middlewares/index.ts
+++ src/domain/budgets/http/middlewares/index.ts
@@ -6,7 +6,20 @@ export const createBudgetMiddleware = (
   res: Response,
   next: NextFunction
 ) => {
-  const CreateBudgetSchema = z.object({});
+  const CreateBudgetSchema = z.object({
+    user_id: z.string().uuid('Invalid UUID format'),
+    name: z
+      .string()
+      .min(1, 'Name is required')
+      .max(30, 'Name must be at most 30 characters long'),
+    percentage: z
+      .number()
+      .min(1, 'Percentage must be at least 1')
+      .max(100, 'Percentage must be at most 100')
+      .int('Percentage must be an integer'),
+    transfer_to_budget: z.number().int('Transfer to budget must be an integer'),
+  });
+
   const result = CreateBudgetSchema.safeParse(req.body);
 
   if (!result.success) {
@@ -14,6 +27,7 @@ export const createBudgetMiddleware = (
       .status(400)
       .json({ status: 'fail', errors: result.error.flatten() });
   }
+
   req.body = result.data;
   next();
 };
