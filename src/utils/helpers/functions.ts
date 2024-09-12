a468c3314ac38c73afef03b7ab035193c3d5994f
MatiMonas <matutin97@gmail.com>
2024-09-12 00:10:17 -0300

    add(utils/helpers/functions): getValidationMessage

---
+++ src/utils/helpers/functions.ts
@@ -0,0 +1,25 @@
+/**
+ * Generates a validation error message for a given property based on its type and optional condition.
+ *
+ * This function creates a message string that describes the validation requirement for a property.
+ * It helps ensure consistency and reusability of error messages across different schemas and validations.
+ *
+ * @param {string} property - The name of the property for which the validation message is being generated.
+ * @param {string} type - The expected type of the property (e.g., 'number', 'string', 'boolean').
+ * @param {string} [condition] - An optional additional condition for the validation message, such as 'positive' or 'non-empty'.
+ *
+ * @returns {string} - A formatted validation message string that describes the expected type and optional condition for the property.
+ *
+ * @example
+ * // Example usage:
+ * const message = getValidationMessage('age', 'number', 'positive');
+ * console.log(message); // Output: 'Property "age" must be a number and positive'
+ */
+export const getValidationMessage = (
+  property: string,
+  type: string,
+  condition?: string
+) =>
+  `Property "${property}" must be ${type}${
+    condition ? ' and ' + condition : ''
+  }`;
