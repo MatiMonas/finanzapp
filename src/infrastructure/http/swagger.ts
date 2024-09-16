390fef201d782f5641a7f7c70a84e1925437edf2
MatiMonas <matutin97@gmail.com>
2024-09-16 19:23:51 -0300

    build: add swagger.yaml

--- src/infrastructure/http/swagger.ts
+++ src/infrastructure/http/swagger.ts
@@ -1,32 +1,12 @@
-import swaggerJSDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+import path from 'path';
 import { Application } from 'express';
 
-const swaggerOptions = {
-  swaggerDefinition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'FinanzApp API',
-      version: '1.0.0',
-      description: 'API Documentation for FinanzApp',
-    },
-    servers: [
-      {
-        url: 'http://localhost:3000',
-        description: 'Local server',
-      },
-    ],
-  },
-  apis: [
-    './src/domain/users/http/router.ts',
-    './src/domain/budgets/http/router.ts',
-  ],
+const swaggerDocument = YAML.load(
+  path.join(__dirname, '../../../swagger.yaml')
+);
+
+export default (app: Application) => {
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 };
-
-const swaggerSpec = swaggerJSDoc(swaggerOptions);
-
-const setupSwagger = (app: Application) => {
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-};
-
-export default setupSwagger;
