30f86349594a99104ecc6b41c3a1326087c7de11
MatiMonas <matutin97@gmail.com>
2025-04-12 23:49:11 -0300

    encapsulate logic at createWage

--- src/components/wages/usecase/index.ts
+++ src/components/wages/usecase/index.ts
@@ -23,31 +23,9 @@ export default class WagesUsecase implements IWagesUsecase {
 
     const data = await modelValidator.validate(payload);
 
-    let monthlyWageSummary;
-    if (!data.monthly_wage_summary_id) {
-      console.log('Creating new monthly wage summary');
-      monthlyWageSummary = await this.wagesRepository.createMonthlyWage({
-        user_id: data.user_id,
-        month_and_year: data.month_and_year,
-        total_wage: data.amount,
-        remaining: data.amount,
-      });
-
-      data.monthly_wage_summary_id = monthlyWageSummary.id;
-    } else {
-      console.log('Updating existing monthly wage summary');
-      await this.wagesRepository.updateMonthlyWageSummary(
-        data.monthly_wage_summary_id,
-        data.amount
-      );
-    }
-
-    const exchangeRate = await this.wagesHttpRepository.getBlueExchangeRate();
-
-    const amountInUSD =
-      data.currency === 'USD' ? data.amount : data.amount / exchangeRate;
-    const amountInARS =
-      data.currency === 'ARS' ? data.amount : data.amount * exchangeRate;
+    const updatedData = await this.handleMonthlyWageSummary(data);
+    const { amountInUSD, amountInARS, exchangeRate } =
+      await this.calculateAmounts(updatedData.amount, updatedData.currency);
 
     await this.wagesRepository.createWage({
       user_id: data.user_id,
@@ -60,9 +38,51 @@ export default class WagesUsecase implements IWagesUsecase {
       monthly_wage_summary_id: data.monthly_wage_summary_id,
     });
 
+    await this.updateBudgetsAllocations(
+      updatedData.user_id,
+      updatedData.amount,
+      updatedData.monthly_wage_summary_id
+    );
+
+    return true;
+  }
+
+  private async handleMonthlyWageSummary(data: any) {
+    if (!data.monthly_wage_summary_id) {
+      const summary = await this.wagesRepository.createMonthlyWage({
+        user_id: data.user_id,
+        month_and_year: data.month_and_year,
+        total_wage: data.amount,
+        remaining: data.amount,
+      });
+
+      data.monthly_wage_summary_id = summary.id;
+    } else
+      await this.wagesRepository.updateMonthlyWageSummary(
+        data.monthly_wage_summary_id,
+        data.amount
+      );
+
+    return data;
+  }
+
+  private async calculateAmounts(amount: number, currency: 'USD' | 'ARS') {
+    const exchangeRate = await this.wagesHttpRepository.getBlueExchangeRate();
+
+    const amountInUSD = currency === 'USD' ? amount : amount / exchangeRate;
+    const amountInARS = currency === 'ARS' ? amount : amount * exchangeRate;
+
+    return { amountInUSD, amountInARS, exchangeRate };
+  }
+
+  private async updateBudgetsAllocations(
+    userId: string,
+    wageAmount: number,
+    monthlyWageSummaryId: number
+  ) {
     const [budgetConfigurations] =
       await this.budgetsRepository.findBudgetConfigurationWhere({
-        user_id: data.user_id,
+        user_id: userId,
         is_active: true,
       });
 
@@ -70,25 +90,17 @@ export default class WagesUsecase implements IWagesUsecase {
 
     await Promise.all(
       budgets.map((budget) => {
-        const percentage = budget.percentage;
-        const remainingAllocation = budget.remaining_allocation;
-
-        const wageAmount = data.amount;
-        const budgetAmount = (percentage / 100) * wageAmount;
+        const budgetAmount = (budget.percentage / 100) * wageAmount;
 
         const updateData = {
           id: budget.id,
-          remaining_allocation: remainingAllocation + budgetAmount,
-          monthly_wage_summary_id: data.monthly_wage_summary_id,
+          remaining_allocation: budget.remaining_allocation + budgetAmount,
+          monthly_wage_summary_id: monthlyWageSummaryId,
           updated_at: new Date(),
         };
 
-        this.budgetsRepository.singuleUpdateBudget(updateData);
-
-        return budget;
+        return this.budgetsRepository.singuleUpdateBudget(updateData);
       })
     );
-
-    return true;
   }
 }
